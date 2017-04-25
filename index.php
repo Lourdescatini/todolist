c8457f99dfca3ece138fd8fbf85ef9f8b840d6a2
Lourdescatini <lourdes.hellen.catini@gmail.com>
2017-04-25 11:59:38 -0300

    arquivos demostração todo list

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+$titulo = ['Dom', 'nobre', 'paladino '];
+$qualidade = ['abridor de galaxias', 'salvador da pátria', 'a colossa de barra do sul'];
+$nome = "lourdes" ;
+
+$titulovizualizado = $titulo[rand(0,2) ];
+$qualidadevizualizado = $qualidade[rand(0,2)];
+
+
+?>
---
+++ logica.php
@@ -0,0 +1,11 @@
+<?php require "logica.php"; ?>
+
+<html>
+
+<head></head>
+
+<body>
+<h1>hola<?php echo "$titulovizualizado $nome,$qualidadevizualizado";?> </h1>
+</body>
+
+</html>
